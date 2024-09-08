7eb3c7de4a4e8bca4bf91e066bd76da806e1e2c3
Eyobed-Tezera <eyobed014@gmail.com>
2024-09-08 07:36:16 -0700

    axios and request

--- netflix-clone/src/components/Header/Header.jsx
+++ netflix-clone/src/components/Header/Header.jsx
@@ -8,9 +8,9 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 function Header() {
   return (
     <div>
-      <div className="header_outlet_container">
-        <div className="header_container">
-          <div className="header_left">
+      <div className="header-outlet-container">
+        <div className="header-container">
+          <div className="header-left">
             <ul>
               <li>
                 <img src={NetflixLogo} alt="Netflix Logo" width="100" />
@@ -23,7 +23,7 @@ function Header() {
               <li>Browse by Language</li>
             </ul>
           </div>
-          <div className="header_right">
+          <div className="header-right">
             <ul>
               <li>
                 <SearchIcon />
