16a16389933ec0e312a15149ec7a6837b0dd7b02
Eyobed-Tezera <eyobed014@gmail.com>
2024-09-11 18:13:58 -0700

    deploy config

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/netflix-clon/'
+  base: "/Netflix-clone-2024/",
   
-})
+});
